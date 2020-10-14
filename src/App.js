cb8fca11632e18e9525289b548c733e542a71332
Nuage <lenuagebrun@gmail.com>
2020-10-14 17:35:46 -0400

    gh-pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Accordions from './components/Accordions/Accordions';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Accordions/>
+    </div>
+  );
+}
+
+export default App;
