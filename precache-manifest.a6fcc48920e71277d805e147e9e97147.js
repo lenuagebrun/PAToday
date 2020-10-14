c90a957566ccf47392ac8050adc87d62287ff515
Nuage <lenuagebrun@gmail.com>
2020-10-14 17:33:00 -0400

    Updates

---
+++ precache-manifest.a6fcc48920e71277d805e147e9e97147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef859825001680fe80465070b7d03c2",
+    "url": "/PA-Today/index.html"
+  },
+  {
+    "revision": "ac4eabe59880d245ee53",
+    "url": "/PA-Today/static/css/main.e615bace.chunk.css"
+  },
+  {
+    "revision": "23725e45139655f286f6",
+    "url": "/PA-Today/static/js/2.de167c2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PA-Today/static/js/2.de167c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4eabe59880d245ee53",
+    "url": "/PA-Today/static/js/main.14c686b1.chunk.js"
+  },
+  {
+    "revision": "5522253d93d7f2243dbc",
+    "url": "/PA-Today/static/js/runtime-main.2b9d4537.js"
+  }
+]);
